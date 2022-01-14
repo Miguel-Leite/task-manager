97ba6cce25be815c277098e0cd8d3f3c2deb9c80
Miguel-Leite <miguelleite200leite@gmail.com>
2022-01-15 00:41:57 +0100

    create navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,10 +1,26 @@
 import { NavLink } from 'react-router-dom'
+import { FiAlignJustify } from "react-icons/fi";
+import { IoDuplicateOutline } from "react-icons/io5";
+
 import '../../styles/navbar.scss'
 
 export const Navbar = () => {
     return (
-        <nav>
-            
-        </nav>
+        <div className="container-fluid">
+            <nav className="navbar">
+                <div className="controls">
+                    <FiAlignJustify className='menu'/>
+                    <NavLink to='/'>
+                        <IoDuplicateOutline />
+                        Add Task
+                    </NavLink>
+                </div>
+
+                <div className="profile">
+                    <h5> Miguel Leite </h5>
+                    <NavLink to='/'> My account </NavLink>
+                </div>
+            </nav>
+        </div>
     )
 }
