5a614ccdcea752b0e5d328f1119efba6dd9c5ac5
Miguel-Leite <miguelleite200leite@gmail.com>
2022-01-15 01:23:10 +0100

    add new component and new router

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route } from 'react-router-dom'
 
 import {Home} from './pages/Home'
 import {Auth} from './pages/Auth'
+import { List } from './pages/List';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
@@ -12,6 +13,7 @@ function App() {
     <BrowserRouter>
       <Route path='/' exact component={Home}></Route>
       <Route path='/auth' component={Auth}></Route>
+      <Route path='/dashboard' component={List}></Route>
     </BrowserRouter>
   );
 }
