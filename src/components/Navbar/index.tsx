b8df67e09567bbbb6a7bd90ff330b7eada4f18e5
Miguel-Leite <miguelleite200leite@gmail.com>
2022-01-15 01:22:44 +0100

    create navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -6,21 +6,19 @@ import '../../styles/navbar.scss'
 
 export const Navbar = () => {
     return (
-        <div className="container-fluid">
-            <nav className="navbar">
-                <div className="controls">
-                    <FiAlignJustify className='menu'/>
-                    <NavLink to='/'>
-                        <IoDuplicateOutline />
-                        Add Task
-                    </NavLink>
-                </div>
+        <nav className="nav">
+            <div className="controls">
+                <FiAlignJustify className='menu'/>
+                <NavLink to='/' className='add'>
+                    <IoDuplicateOutline />
+                    Add Task
+                </NavLink>
+            </div>
 
-                <div className="profile">
-                    <h5> Miguel Leite </h5>
-                    <NavLink to='/'> My account </NavLink>
-                </div>
-            </nav>
-        </div>
+            <div className="profile">
+                <h5> Miguel Leite </h5>
+                <NavLink to='/'> My account </NavLink>
+            </div>
+        </nav>
     )
 }
